ff847a0d55738e494d42ac01cdea7473b57c491a
VadimBoz <werbel34@mail.ru>
2023-12-01 21:05:01 +0300

    calculator

--- SimpleCalculator/src/Main.java
+++ SimpleCalculator/src/Main.java
@@ -4,7 +4,6 @@ import controller.IController;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.util.logging.config.file"));
         IController controller = new Controller();
         controller.startProg();
     }
